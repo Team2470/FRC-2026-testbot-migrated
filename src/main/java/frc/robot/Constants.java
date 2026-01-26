516bb5e1f75b1015f9599b25bea71cd36bc2c0f5
MrGotGame <jfgindt@mtu.edu>
2026-01-26 17:23:22 -0600

    Changed Location of pass targets to be closer to driver stations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,10 +22,10 @@ public class Constants {
         // Target Locations
         public static final Translation2d BLUE_HUB_LOCATION         = new Translation2d(4.600, 4.025);
         public static final Translation2d RED_HUB_LOCATION          = new Translation2d(12.000, 4.025);
-        public static final Translation2d BLUE_RIGHT_PASS_LOCATION  = new Translation2d(1.500, 2.000);
-        public static final Translation2d BLUE_LEFT_PASS_LOCATION   = new Translation2d(1.500, 6.000);
-        public static final Translation2d RED_LEFT_PASS_LOCATION    = new Translation2d(15.000, 2.000);
-        public static final Translation2d RED_RIGHT_PASS_LOCATION   = new Translation2d(15.000, 6.000);
+        public static final Translation2d BLUE_RIGHT_PASS_LOCATION  = new Translation2d(0.500, 2.000);
+        public static final Translation2d BLUE_LEFT_PASS_LOCATION   = new Translation2d(0.500, 6.000);
+        public static final Translation2d RED_LEFT_PASS_LOCATION    = new Translation2d(16.000, 2.000);
+        public static final Translation2d RED_RIGHT_PASS_LOCATION   = new Translation2d(16.000, 6.000);
         public static final Translation2d HUB_LOCATION              = isBlueAlliance() ? BLUE_HUB_LOCATION : RED_HUB_LOCATION;
         public static final Translation2d PASS_LEFT_LOCATION        = isBlueAlliance() ? BLUE_LEFT_PASS_LOCATION : RED_LEFT_PASS_LOCATION;
         public static final Translation2d PASS_RIGHT_LOCATION       = isBlueAlliance() ? BLUE_RIGHT_PASS_LOCATION : RED_RIGHT_PASS_LOCATION;
